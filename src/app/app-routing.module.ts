5c8655aadb5ce6af86a3a08ad3c5d9eb34271b89
ANANTH4004 <balamuruganananth@gmail.com>
2023-01-05 01:20:07 +0530

    skills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
-  
+  {path:'',component:HomeComponent},
   {path:"home" , component:HomeComponent},
   {path:'contact' , component: ContactComponent},
+  {path:'skills' , component:SkillsComponent}
  
 ];
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
