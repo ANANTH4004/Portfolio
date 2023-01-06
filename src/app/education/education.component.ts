34a1729a1c1688d3e3177350342233fe1be90fec
ANANTH4004 <balamuruganananth@gmail.com>
2023-01-07 00:11:35 +0530

    responsive education

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -5,24 +5,7 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  // animations: [
-  //   trigger('paragraphAnimation', [
-  //     state('invisible', style({
-  //       opacity: 0,
-  //       transform: 'translateX(-100%)'
-  //     })),
-  //     state('visible', style({
-  //       opacity: 1,
-  //       transform: 'translateX(0)'
-  //     })),
-  //     transition('invisible => visible', animate(500))
-  //   ])
-  // ],
-  styleUrls: ['./education.component.scss']
-  
-  
-  
-  
+  styleUrls: ['./education.component.scss'] 
 })
 export class EducationComponent implements OnInit{
 
